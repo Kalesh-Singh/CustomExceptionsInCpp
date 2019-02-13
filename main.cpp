a739ec619f7a19f090ebcda732f01649cc505446
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2019-02-13 13:22:56 -0800

    Example Custom Exception in C++

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <exception>
+#include <sstream>
+
+class MalformedInstructionException : public std::exception {
+public:
+    explicit MalformedInstructionException() = default;;
+
+    explicit MalformedInstructionException(const char* msg)
+    : msg(msg) {}
+    const char* what() {
+        return (this->msg != nullptr) ? msg : nullptr;
+    }
+private:
+    const char* msg = nullptr;
+};
+
+int main() {
+    std::string inst = "a CHAR a b";
+
+    std::stringstream ss;
+    ss << inst;
+
+    int pc;
+    ss >> pc;
+    try {
+        if (ss.fail()) {
+            throw MalformedInstructionException("Invalid Op Code");
+        }
+    } catch (MalformedInstructionException &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
